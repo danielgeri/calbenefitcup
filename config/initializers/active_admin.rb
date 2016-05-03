685a79b426072335ceba1a3d759174ac3a98e437
Jay Yee <fmo@opmbx.org>
2016-05-03 18:36:37 -0400

    Add more setup to activeadmin

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -4,7 +4,7 @@ ActiveAdmin.setup do |config|
   # Set the title that is displayed on the main layout
   # for each of the active admin pages.
   #
-  config.site_title = "Calbenefitcup"
+  config.site_title = "Cal Benefit Cup"
 
   # Set the link url for the title. For example, to take
   # users to your main site. Defaults to no link.
@@ -190,6 +190,7 @@ ActiveAdmin.setup do |config|
   #
   # To load a javascript file:
   #   config.register_javascript 'my_javascript.js'
+  config.register_javascript 'ckeditor/init.js'
 
   # == CSV options
   #
