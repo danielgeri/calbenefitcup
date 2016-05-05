efbf926000c81c6ca0939c77f61ca59e8a0fd30c
Jay Yee <fmo@opmbx.org>
2016-05-05 01:40:50 -0400

    Add validation of permalink

--- app/models/page.rb
+++ app/models/page.rb
@@ -3,7 +3,14 @@ class Page < ActiveRecord::Base
   belongs_to :parent_page, class_name: 'Page'
 
   validates_uniqueness_of :permalink
+  validates_presence_of :permalink, :title
 
+  before_validation :permalink_from_title
+  
+  def permalink_from_title
+    self.permalink = self.title.parameterize if self.permalink == ''
+  end
+  
   def to_param
     permalink
   end
