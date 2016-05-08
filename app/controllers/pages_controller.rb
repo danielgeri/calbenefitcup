a6359eefce1daa5eea53097fd233d73a0b3d8cef
Jay Yee <fmo@opmbx.org>
2016-05-08 04:58:52 -0400

    Refactor + augment Pages functionality
    - now redirects if there's a redirect
    - errors if the page is a draft

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,17 +1,17 @@
 class PagesController < ApplicationController
   def show
+    @page = Page.includes(:headlines, :images)
+                .where(is_draft: false)
+                .find_by_permalink!(params[:id])
+    redirect_to @page.forward_url unless @page.forward_url.nil?
+    @images = @page.images.empty? ? nil : @page.images.order(:slide_index)
+    @headlines = @page.headlines.empty? ? nil :
+                   @page.headlines.order(published_on: :desc)
+    @nav = Page.includes(:subpages)
+               .where(is_draft: false, is_displayed: true)
+               .order(:menu_index)
     @meet = Meet.find_by(is_current: true)
     @meet_range = @meet.date_range if @meet
-    @page = Page.includes(:headlines, :images).find_by_permalink!(params[:id])
-    unless @page.images.empty?
-      @images = @page.images.order(:slide_index)
-    end
-    unless @page.headlines.empty?
-      @headlines = @page.headlines.order(published_on: :desc)
-    end
-    @nav = Page.includes(:subpages).
-           where(is_draft: false, is_displayed: true).
-           order(:menu_index)
     # respond_to do |format|
     #   format.html # show.html.erb
     #   format.json { render json: @page }
