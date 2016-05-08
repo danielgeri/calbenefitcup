b9d67500af7bc6692d29bd8f198785cf671f879e
Jay Yee <fmo@opmbx.org>
2016-05-08 00:04:38 -0400

    Process images + headlines for pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,13 +2,19 @@ class PagesController < ApplicationController
   def show
     @meet = Meet.find_by(is_current: true)
     @meet_range = @meet.date_range if @meet
-    @page = Page.find_by_permalink!(params[:id])
+    @page = Page.includes(:headlines, :images).find_by_permalink!(params[:id])
+    unless @page.images.empty?
+      @images = @page.images.order(:slide_index)
+    end
+    unless @page.headlines.empty?
+      @headlines = @page.headlines.order(published_on: :desc)
+    end
     @nav = Page.includes(:subpages).
            where(is_draft: false, is_displayed: true).
            order(:menu_index)
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @page }
-    end
+    # respond_to do |format|
+    #   format.html # show.html.erb
+    #   format.json { render json: @page }
+    # end
   end
 end
