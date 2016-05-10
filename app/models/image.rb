25c25fdf02b1d6d3d94df5faf924a240a87237e6
Jay Yee <fmo@opmbx.org>
2016-05-10 16:06:24 -0400

    Add a thumnail option to image uploads

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,5 +1,6 @@
 class Image < ActiveRecord::Base
   belongs_to :page
-  has_attached_file :image_location, styles: { orbit: '800x800>' }
+  has_attached_file :image_location,
+                    styles: { orbit: '800x800>', thumb: '200x200>' }
   validates_attachment_content_type :image_location, content_type: /\Aimage/
 end
